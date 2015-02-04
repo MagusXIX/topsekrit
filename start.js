4fc332cb932c00db7ada5a7855b71497672ced7e
MagusXIX <magusxix@gmail.com>
2015-02-04 15:19:33 -0800

    Added Backbone to dependencies
    Green, up, up.

--- start.js
+++ start.js
@@ -9,9 +9,12 @@ var Restaurant = require("./models/restaurant.js");
 
 var startServer = function () {
 
-  //Turn on the controllers.
+  //BEFORE STARTING THE SERVER
+
+  //Check to see if we have restaurants already, if not then create some.
   db.restaurants.find({}, function (err, restaurants) {
     if (err) {
+      console.log("WHY WOULD YOU DO THIS?");
       console.log(err);
     } else if (restaurants.length > 0) {
       console.log("Basic restaurants already created, skipping restaurant creation.");
@@ -21,7 +24,7 @@ var startServer = function () {
     }
   })
 
-  //Start the router.  Server technically starts inside the router via http.createServer(); ... don't ask.
+  //START THE ROUTER/SERVER  Server technically starts inside the router via http.createServer(); ... don't ask.
   router.router();
 
 }
