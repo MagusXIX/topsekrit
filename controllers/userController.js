9ede1e1f3fc7ce77f1311d1b28ffe912e1aad101
MagusXIX <magusxix@gmail.com>
2015-02-04 12:37:13 -0800

    Added routing for imgs, css
    Could be further streamlined so that each image doesn't need to be
    routed individually.

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,14 +9,65 @@ var respond = function(request, response, write){
   response.end();
 }
 
-var index = function (request, response) {
-  fs.readFile("./views/statics/mainView.html", function (err, data) {
-    if (err) {
-      console.log("YO! WE HAVE AN ERROR OVER HERE!");
-      console.log(err);
-    }
-    respond(request, response, data);
-  });
+var functions = {
+  index: function (request, response) {
+    fs.readFile("./views/statics/mainView.html", function (err, data) {
+      if (err) {
+        console.log("YO! WE HAVE AN ERROR OVER HERE!");
+        console.log(err);
+      }
+      respond(request, response, data);
+    });
+  },
+
+
+  //------
+  //IMAGES
+  //------
+  doubler: function (request, response) {
+    fs.readFile("./views/css/images/doubler.jpg", function (err, data) {
+      if (err) {
+        console.log("YO! WE HAVE AN ERROR OVER HERE!");
+        console.log(err);
+      }
+      respond(request, response, data);
+    })
+  },
+
+  wilkinson: function (request, response) {
+    fs.readFile("./views/css/images/wilkinson.jpg", function (err, data) {
+      if (err) {
+        console.log("YO! WE HAVE AN ERROR OVER HERE!");
+        console.log(err);
+      }
+      respond(request, response, data);
+    })
+  },
+
+  fiveStar: function (request, response) {
+    fs.readFile("./views/css/images/fiveStar.png", function (err, data) {
+      if (err) {
+        console.log("YO! WE HAVE AN ERROR OVER HERE!");
+        console.log(err);
+      }
+      respond(request, response, data);
+    })
+  },
+
+  //-----------
+  //STYLESHEETS
+  //-----------
+  stylus: function (request, response) {
+    fs.readFile("./views/css/stylus.css", function (err, data) {
+      if (err) {
+        console.log("YO! WE HAVE AN ERROR OVER HERE!");
+        console.log(err);
+      }
+      response.setHeader('content-type', 'text/css');
+      respond(request, response, data);
+    })
+  },
+
 }
 
-exports.index = index;
+exports.functions = functions;
--- router.js
+++ router.js
@@ -8,7 +8,7 @@ var url = require("url");
 var qs = require("querystring");
 var userController = require("./controllers/userController.js");
 
-//Starting the Server
+//Routes
 var router = function () {
   var onRequest = function (request, response) {
     console.log("Request received.");
@@ -16,11 +16,35 @@ var router = function () {
     var params = url.parse(request.url).query;
     
     if (pathname == "/") {
-      userController.index(request, response);
+      userController.functions.index(request, response);
+    }
+
+
+    //------
+    //IMAGES
+    //------
+    if (pathname == "/doubler.jpg") {
+      userController.functions.doubler(request, response);
+    }
+
+    if (pathname == "/wilkinson.jpg") {
+      userController.functions.wilkinson(request, response);
+    }
+
+    if (pathname == "/fiveStar.png") {
+      userController.functions.fiveStar(request, response);
+    }
+
+    //-----------
+    //STYLESHEETS
+    //-----------
+    if (pathname == "/css/stylus.css") {
+      userController.functions.stylus(request, response);
     }
 
   }
   
+  //Starting the server.
   var server = http.createServer(onRequest).listen(serverport, myAddress);
   console.log("Server is running on port " + serverport + ".");
   
